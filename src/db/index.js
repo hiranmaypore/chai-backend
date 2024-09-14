603479f81b647ba4bf25de36e0bd4fdd85a25ffa
hiranmay pore <hiranmaypore4@gmail.com>
2024-09-14 12:14:56 +0530

    new commit

--- src/db/index.js
+++ src/db/index.js
@@ -3,14 +3,11 @@ import { DB_NAME } from "../constant.js";
 
 const connectDB = async  ()=> {
     try {
-        const uri = (`${process.env.DATABASE_URI}/${DB_NAME}`);
+        const uri = `${process.env.MONGODB_URI}/${DB_NAME}`;
         if (!uri) {
-            throw new Error('DATABASE_URI is not defined');
+            throw new Error('Mongo Uri is not defined');
         }
-        const connectionInstant  = await mongoose.connect(uri, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
+        const connectionInstant  = await mongoose.connect(uri);
         console.log(`MongoDB connected successfully. DB HOST: ${connectionInstant.connection.host}`);
     } catch (error) {
         console.error('Error connecting to MongoDB:', error);
