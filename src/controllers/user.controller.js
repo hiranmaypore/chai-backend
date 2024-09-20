952608130e89876b10dff2a64072dbe6299a840e
hiranmay pore <hiranmaypore4@gmail.com>
2024-09-21 02:00:26 +0530

    usercontroller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,33 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import{Apierror} from "../utils/Apierror.js"
+import {User} from "../models/user.model.js"
 
 const registerUser = asyncHandler( async (req,res) => {
-    res.status(200).json({
-        message: 'hiranmay'
+    // get user details from frontend
+    // validation - not empty
+    // check if user already exist: through username and email
+    // check for images
+    // check for avatar
+    // upload them to cloudinary,avatar
+    // remove password and refresh token field from response 
+    // check for user creation
+    // return response
+    const {fullname, email, username, password} =  req.body
+    console.log("email:",email);
+    if (
+        [fullname, email, username, password].some((field) => field?.trim() === ''))
+        {
+            throw new Apierror(400,'All fields are required')
+        }
+    const existedUser = User.findOne({
+        $or: [{username},{email}]
     })
+
+    if(existedUser){
+        throw new Apierror(409,'User already exist')
+    }
+
+    req.files?.avatar[0]?.path
 } )
 
-export{registerUser}
+export{registerUser,}
